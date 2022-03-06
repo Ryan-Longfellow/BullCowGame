821359ce1ed188d8304264a0b6e5d0cb876df399
Ryan Longfellow <30578561+Ryan-Longfellow@users.noreply.github.com>
2022-03-06 10:50:58 -0600

    Move HiddenWord  and add input check

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -5,11 +5,22 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     PrintLine(TEXT("Welcome to Bull Cows!"));
+    PrintLine(TEXT("Guess the 4 letter word")); // Magic number remove!!
     PrintLine(TEXT("Press enter to continue..."));
+    HiddenWord = TEXT("cake");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = TEXT("cake");
+
+    // Check if input from user matches the hidden word
+    if (Input == HiddenWord)
+    {
+        PrintLine(TEXT("You win!"));
+    }
+    else
+    {
+        PrintLine(TEXT("You lose!"));
+    }
 }
