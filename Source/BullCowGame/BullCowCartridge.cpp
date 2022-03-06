23b2e1f84497f1aea294a8eeccfa1857b336701e
Ryan Longfellow <30578561+Ryan-Longfellow@users.noreply.github.com>
2022-03-06 15:43:15 -0600

    Add pseudocode and a string length check
    Pseudocode is for the rest of the game loop
    
    String length comparison is between HiddenWord and user Input to ensure they are the same

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,23 +4,56 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+
+    // Welcome the player
     PrintLine(TEXT("Welcome to Bull Cows!"));
     PrintLine(TEXT("Guess the 4 letter word")); // Magic number remove!!
     PrintLine(TEXT("Press enter to continue..."));
-    HiddenWord = TEXT("cake");
+
+    SetupGame();
+
+    // Prompt player for guess
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
 
-    // Check if input from user matches the hidden word
+    // Checking PlayerGuess
+
+    // Check if isogram
+    // Prompt to GuessAgain
+    
+
+    // Subtract life
+    // Show lives left
+    // Check if lives > 0
+    // If yes GuessAgain
+    // If no show GameOver and HiddenWord
+    // Press Enter to PlayAgain
+    // Check User Input
+    // PlayAgain or Quit
+
+    // Check if Input from user matches HiddenWord
     if (Input == HiddenWord)
     {
         PrintLine(TEXT("You win!"));
     }
     else
     {
+        // Check if right number of characters
+        if (Input.Len() != HiddenWord.Len())
+        {
+            PrintLine(TEXT("The Hidden Word is 4 characters long, try again!")); // Magic number remove!!
+            // Prompt to GuessAgain
+        }
+
         PrintLine(TEXT("You lose!"));
     }
+}
+
+void UBullCowCartridge::SetupGame()
+{
+    HiddenWord = TEXT("cake");
+    Lives = 4;
 }
